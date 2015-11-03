3c464503bd45637619f18b7115d73bb26b6eaeed
Chris S <mrshrinkray@gmail.com>
2015-11-03 19:06:18 +0000

    Version 1.0.0 - nuspec and installer.

---
+++ chromedriver-latest/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'chromedriver-latest' # arbitrary name for the package, used in messages
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$chromedriverVersion = (wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE).Content
+$url = "https://chromedriver.storage.googleapis.com/$chromedriverVersion/chromedriver_win32.zip"
+$url64 = $url
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  url           = $url
+  url64bit      = $url64
+}
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
