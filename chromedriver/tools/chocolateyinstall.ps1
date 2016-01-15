246849b224e65d7023a60cd0df1b0759007b3fa1
Chris S <mrshrinkray@gmail.com>
2016-01-15 09:06:20 +0000

    v1.0.1: Fixed issue with wget ('internet explorer not available' bug)

--- chromedriver/tools/chocolateyinstall.ps1
+++ chromedriver/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@
 $packageName = 'chromedriver'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$chromedriverVersion = (wget http://chromedriver.storage.googleapis.com/LATEST_RELEASE).Content
+$chromedriverVersion = (wget -UseBasicParsing http://chromedriver.storage.googleapis.com/LATEST_RELEASE).Content
 $url = "https://chromedriver.storage.googleapis.com/$chromedriverVersion/chromedriver_win32.zip"
 $url64 = $url
 
