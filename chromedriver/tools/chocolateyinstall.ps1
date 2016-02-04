e4972b2f95ca03962b360f44bb801cda8fd4eaf8
Chris S <mrshrinkray@gmail.com>
2016-02-04 19:54:32 +0000

    Update chocolateyinstall.ps1

--- chromedriver/tools/chocolateyinstall.ps1
+++ chromedriver/tools/chocolateyinstall.ps1
@@ -5,14 +5,5 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $chromedriverVersion = (wget -UseBasicParsing http://chromedriver.storage.googleapis.com/LATEST_RELEASE).Content
 $url = "https://chromedriver.storage.googleapis.com/$chromedriverVersion/chromedriver_win32.zip"
-$url64 = $url
-
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
-  url           = $url
-  url64bit      = $url64
-}
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir
