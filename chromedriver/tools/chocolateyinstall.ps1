4c4e2bc985e88bbe667ae9f950bec652d8ae886a
Chris S <mrshrinkray@gmail.com>
2016-02-04 23:00:06 +0000

    Used WebClient to get the latest chrome version as a string.

--- chromedriver/tools/chocolateyinstall.ps1
+++ chromedriver/tools/chocolateyinstall.ps1
@@ -3,7 +3,8 @@
 $packageName = 'chromedriver'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$chromedriverVersion = (wget -UseBasicParsing http://chromedriver.storage.googleapis.com/LATEST_RELEASE).Content
+$webClient = New-Object System.Net.WebClient
+$chromedriverVersion = $webClient.DownloadString("http://chromedriver.storage.googleapis.com/LATEST_RELEASE").Trim()
 $url = "https://chromedriver.storage.googleapis.com/$chromedriverVersion/chromedriver_win32.zip"
 
 Install-ChocolateyZipPackage $packageName $url $toolsDir
