a0d56ca008a470b24ee989a8eaf74a5986a4efda
Chris S <mrshrinkray@gmail.com>
2016-02-09 20:55:22 +0000

    Versioned to 2.2.1

--- chromedriver/tools/chocolateyinstall.ps1
+++ chromedriver/tools/chocolateyinstall.ps1
@@ -1,10 +1,11 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = "Stop";
 
-$packageName = 'chromedriver'
+$packageName = "chromedriver"
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$checkSum = "2EE59A8047EC345F886D5B90656DBEA1469B7E44"
 
-$webClient = New-Object System.Net.WebClient
-$chromedriverVersion = $webClient.DownloadString("http://chromedriver.storage.googleapis.com/LATEST_RELEASE").Trim()
+# The latest version number can be found at http://chromedriver.storage.googleapis.com/LATEST_RELEASE
+$chromedriverVersion = "2.21"
 $url = "https://chromedriver.storage.googleapis.com/$chromedriverVersion/chromedriver_win32.zip"
 
-Install-ChocolateyZipPackage $packageName $url $toolsDir
+Install-ChocolateyZipPackage "packageName" -url "$url" -unzipLocation "$toolsDir" -checksumType "sha1" -checksum "$checkSum"
