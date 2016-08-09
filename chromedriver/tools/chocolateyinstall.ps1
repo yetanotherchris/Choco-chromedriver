11a73cf6b7a66bc3039995b3588e1d526cd946ad
Chris S <mrshrinkray@gmail.com>
2016-08-09 15:27:46 +0100

    feature: Version 2.23

--- chromedriver/tools/chocolateyinstall.ps1
+++ chromedriver/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $checkSum = "128F4EA29870D5F7C1235403E2DEED13DC0903A7"
 
 # The latest version number can be found at http://chromedriver.storage.googleapis.com/LATEST_RELEASE
-$chromedriverVersion = "2.22"
+$chromedriverVersion = "2.23"
 $url = "https://chromedriver.storage.googleapis.com/$chromedriverVersion/chromedriver_win32.zip"
 
 Install-ChocolateyZipPackage "packageName" -url "$url" -unzipLocation "$toolsDir" -checksumType "sha1" -checksum "$checkSum"
