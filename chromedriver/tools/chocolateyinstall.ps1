5810957bc7f4dd4de65573c318af57ed737f3438
Chris S <mrshrinkray@gmail.com>
2016-10-30 10:54:31 +0000

    Version 2.25

--- chromedriver/tools/chocolateyinstall.ps1
+++ chromedriver/tools/chocolateyinstall.ps1
@@ -1,11 +1,12 @@
 ﻿$ErrorActionPreference = "Stop";
 
+# http://onlinemd5.com/ to generate the SHA1
 $packageName = "chromedriver"
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$checkSum = "76739B1CEE5E21574CE33046C9D455F0984456EB"
+$checkSum = "D0B6BE10D4FFCE4CE338D0E31AA71F80A95B4C14"
 
 # The latest version number can be found at http://chromedriver.storage.googleapis.com/LATEST_RELEASE
-$chromedriverVersion = "2.24"
+$chromedriverVersion = "2.25"
 $url = "https://chromedriver.storage.googleapis.com/$chromedriverVersion/chromedriver_win32.zip"
 
 Install-ChocolateyZipPackage "packageName" -url "$url" -unzipLocation "$toolsDir" -checksumType "sha1" -checksum "$checkSum"
