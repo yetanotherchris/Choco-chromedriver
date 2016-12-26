b902c8439c62ada23a138cb601bca53bad277219
Chris S <mrshrinkray@gmail.com>
2016-12-26 14:24:22 +0000

    appveyor automatic chocolateyness

--- chromedriver/tools/chocolateyinstall.template.ps1
+++ chromedriver/tools/chocolateyinstall.template.ps1
@@ -1,12 +1,11 @@
 ﻿$ErrorActionPreference = "Stop";
 
-# http://onlinemd5.com/ to generate the SHA1
 $packageName = "chromedriver"
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$checkSum = "D0B6BE10D4FFCE4CE338D0E31AA71F80A95B4C14"
+$checkSum = "{CHECKSUM}"
 
 # The latest version number can be found at http://chromedriver.storage.googleapis.com/LATEST_RELEASE
-$chromedriverVersion = "2.25"
+$chromedriverVersion = "{VERSION}"
 $url = "https://chromedriver.storage.googleapis.com/$chromedriverVersion/chromedriver_win32.zip"
 
 Install-ChocolateyZipPackage "packageName" -url "$url" -unzipLocation "$toolsDir" -checksumType "sha1" -checksum "$checkSum"
