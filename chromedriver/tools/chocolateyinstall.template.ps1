e38ad5bdc9c88d5b6700d2c7f7a6cf5fdeb9f59c
Chris S <chrisasmall+github@gmail.com>
2023-09-05 10:17:11 +0100

    Remove old version check from choco install ps1

--- chromedriver/tools/chocolateyinstall.template.ps1
+++ chromedriver/tools/chocolateyinstall.template.ps1
@@ -6,27 +6,17 @@ $checkSum = "{CHECKSUM}"
 $chromedriverVersion = "{VERSION}"
 $desiredPlatform = "win32"
 
-$desiredVersionParsed = [version]$chromedriverVersion
-$firstNewApiVersion = [version]"115.0.5763.0" # for now the first version which shows chromedriver download urls 
-# check if we need to use the old style fetch or the new api
-if ($desiredVersionParsed -lt $firstNewApiVersion) {
-  # The latest version number for the old api can be found at http://chromedriver.storage.googleapis.com/LATEST_RELEASE
-  $chromedriverVersion = "{VERSION}"
-  $url = "https://chromedriver.storage.googleapis.com/$chromedriverVersion/chromedriver_$desiredPlatform.zip"
-  Install-ChocolateyZipPackage "packageName" -url "$url" -unzipLocation "$toolsDir" -checksumType "sha256" -checksum "$checkSum"
-} else {
-  # Use the new google API
-  # URL to fetch JSON content from
-  $jsonUrl = "https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json"
-  # Fetch the JSON content from the URL
-  $jsonContent = Invoke-RestMethod -Uri $jsonUrl
-  # Find the desired versioninfo
-  $desiredVersionInfo = $jsonContent.versions | Where-Object { $_.version -eq $chromedriverVersion }
-  
-  # Find the Chromedriver download URL for the desired platform
-  $chromedriverUrl = $desiredVersionInfo.downloads.$packageName | 
-      Where-Object { $_.platform -eq $desiredPlatform } | 
-      Select-Object -ExpandProperty url
-  
-  Install-ChocolateyZipPackage "packageName" -url "$chromedriverUrl" -unzipLocation "$toolsDir" -checksumType "sha256" -checksum "$checkSum"
-}
+# Use the new google API
+# URL to fetch JSON content from
+$jsonUrl = "https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json"
+# Fetch the JSON content from the URL
+$jsonContent = Invoke-RestMethod -Uri $jsonUrl
+# Find the desired versioninfo
+$desiredVersionInfo = $jsonContent.versions | Where-Object { $_.version -eq $chromedriverVersion }
+
+# Find the Chromedriver download URL for the desired platform
+$chromedriverUrl = $desiredVersionInfo.downloads.$packageName | 
+    Where-Object { $_.platform -eq $desiredPlatform } | 
+    Select-Object -ExpandProperty url
+
+Install-ChocolateyZipPackage "packageName" -url "$chromedriverUrl" -unzipLocation "$toolsDir" -checksumType "sha256" -checksum "$checkSum"
