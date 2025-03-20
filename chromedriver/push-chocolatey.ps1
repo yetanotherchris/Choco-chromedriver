7cc2e08086d655607571d0533577ddec074da303
Chris <frown-wizard-hug@duck.com>
2025-03-20 21:24:04 +0000

    Update push-chocolatey.ps1

--- chromedriver/push-chocolatey.ps1
+++ chromedriver/push-chocolatey.ps1
@@ -59,7 +59,6 @@ $content = $content.Replace("{VERSION}", $versionNumber)
 Remove-Item "$currentDir/chocolateyinstall.template.ps1"
 
 # Install chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
