5e968e929851300b89a5d634aa6e91841e193d9a
Wai-Wai Ng <waiwai95@gmail.com>
2025-03-20 20:03:13 +0000

    Support win64 (#14)

--- chromedriver/push-chocolatey.ps1
+++ chromedriver/push-chocolatey.ps1
@@ -11,29 +11,43 @@
 )
 
 $ErrorActionPreference = "Stop"
-$desiredPlatform = "win32"
+$win32Platform = "win32"
+$win64Platform = "win64"
 
 pushd ./chromedriver/tools
 
 # Download the version
 # URL of the JSON file
 $jsonUrl = "https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions-with-downloads.json"
+
 # Fetch JSON content
 $jsonContent = Invoke-RestMethod -Uri $jsonUrl
-$url = $jsonContent.channels.Stable.downloads.chromedriver | Where-Object { $_.platform -eq $desiredPlatform } | Select-Object -ExpandProperty url
-Write-Host "Downloading $url"
-wget "$url" -OutFile chromedriver_win32.zip
+$url32 = $jsonContent.channels.Stable.downloads.chromedriver | Where-Object { $_.platform -eq $win32Platform } | Select-Object -ExpandProperty url
+$url64 = $jsonContent.channels.Stable.downloads.chromedriver | Where-Object { $_.platform -eq $win64Platform } | Select-Object -ExpandProperty url
+
+Write-Host "Downloading $url32"
+wget "$url32" -OutFile chromedriver_win32.zip
 
 # Get its checksum
-$hash = Get-FileHash chromedriver_win32.zip
-$hash = $hash.Hash
-Write-Host "Hash is: $hash"
+$hash32 = Get-FileHash chromedriver_win32.zip
+$hash32 = $hash32.Hash
+Write-Host "Hash is: $hash32"
 rm chromedriver_win32.zip
+
+Write-Host "Downloading $url64"
+wget "$url64" -OutFile chromedriver_win64.zip
+
+# Get its checksum
+$hash64 = Get-FileHash chromedriver_win64.zip
+$hash64 = $hash64.Hash
+Write-Host "Hash is: $hash64"
+rm chromedriver_win64.zip
 
 # Replace the checksum and version in the chocolateyinstall.ps1 file
 $pwd = pwd
 $content = [IO.File]::ReadAllText("$pwd\chocolateyinstall.template.ps1")
-$content = $content.Replace("{CHECKSUM}", $hash)
+$content = $content.Replace("{CHECKSUM32}", $hash32)
+$content = $content.Replace("{CHECKSUM64}", $hash64)
 $content = $content.Replace("{VERSION}", $versionNumber)
 
 [IO.File]::WriteAllText("$pwd\chocolateyinstall.ps1", $content)
